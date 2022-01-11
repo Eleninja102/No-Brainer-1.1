64bf50d4d9e7a9d05515effe65cf9c3c82cb89ce
Coleton Watt <electricninja102@gmail.com>
2022-01-10 22:18:21 -0700

    Initial Commit

---
+++ No Brainer 1.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  No Brainer 1.1
+//
+//  Created by Coleton Watt on 1/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
