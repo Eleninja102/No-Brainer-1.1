11d557e855cf08ceef064c0a12c7e9826bef7ccf
Coleton Watt <electricninja102@gmail.com>
2022-01-18 14:28:20 -0700

    Templated Class

--- No Brainer 1.1/Point.cpp
+++ No Brainer 1.1/Point.cpp
@@ -8,39 +8,3 @@
 #include "Point.hpp"
 
 
-Point Point::operator+(const Point &toAdd){
-    Point thirdPoint(x + toAdd.x, y + toAdd.y);
-    
-    return thirdPoint;
-}
-Point Point::operator+=(const Point &toAdd){
-    x += toAdd.x;
-    y += toAdd.y;
-    
-    return *this;
-}
-
-Point Point::operator-(const Point &toSubtract){
-    Point thirdPoint(x - toSubtract.x, y - toSubtract.y);
-    
-    return thirdPoint;
-}
-Point Point::operator-=(const Point &toSubtract){
-    x -= toSubtract.x;
-    y -= toSubtract.y;
-    
-    return *this;
-}
-
-ostream& operator << (ostream &out, const Point &toPrint){
-    out << "(" << toPrint.x << ", " << toPrint.y << ")" << endl;
-    
-    return out;
-}
-
-istream& operator >> (istream &in, Point &toInsert){
-    cout << "Enter x and y cord" << endl;
-    in >> toInsert.x >> toInsert.y;
-    
-    return in;
-}
--- No Brainer 1.1/Point.hpp
+++ No Brainer 1.1/Point.hpp
@@ -12,25 +12,80 @@
 
 using namespace std;
 
+template <class Type>
+class Point;
+
+template <class Type>
+ostream& operator << (ostream &, Point<Type> &);
+
+template <class Type>
+
+istream& operator >> (istream &, Point<Type> &);
+
+template <class Type>
 class Point{
 private:
-    int x;
-    int y;
+    Type x;
+    Type y;
 
 public:
     Point(){x=0; y=0;}
-    Point(int x, int y): x(x), y(y){}
-    Point operator+(const Point &);
-    Point operator+=(const Point &);
+    Point(Type x, Type y): x(x), y(y){}
+    Point<Type> operator+(const Point<Type> &toAdd);
+    Point<Type> operator+=(const Point<Type> &toAdd);
     
-    Point operator-(const Point &);
-    Point operator-=(const Point &);
+    Point<Type> operator-(const Point<Type> &);
+    Point<Type> operator-=(const Point<Type> &);
     
-    friend ostream& operator << (ostream &, const Point &);
-    friend istream& operator >> (istream &, Point &);
+    friend ostream& operator << <>(ostream &, Point<Type> &);
+    friend istream& operator >> <>(istream &, Point<Type> &);
 
 
 };
+template <class Type>
+Point<Type> Point<Type>::operator+(const Point<Type> &toAdd){
+    Point<Type> thirdPoint(x + toAdd.x, y + toAdd.y);
+    
+    return thirdPoint;
+}
+
+template <class Type>
+Point<Type> Point<Type>::operator+=(const Point<Type> &toAdd){
+    x += toAdd.x;
+    y += toAdd.y;
+    
+    return *this;
+}
+
+template <class Type>
+Point<Type> Point<Type>::operator-(const Point<Type> &toSubtract){
+    Point<Type> thirdPoint(x - toSubtract.x, y - toSubtract.y);
+    
+    return thirdPoint;
+}
+
+template <class Type>
+Point<Type> Point<Type>::operator-=(const Point<Type> &toSubtract){
+    x -= toSubtract.x;
+    y -= toSubtract.y;
+    
+    return *this;
+}
+
+template <class Type>
+ostream& operator << (ostream &out, Point<Type> &toPrint){
+    out << "(" << toPrint.x << ", " << toPrint.y << ")" << endl;
+    
+    return out;
+}
+
+template <class Type>
+istream& operator >> (istream &in, Point<Type> &toInsert){
+    cout << "Enter x and y cord" << endl;
+    in >> toInsert.x >> toInsert.y;
+    
+    return in;
+}
 
 
 #endif /* Point_hpp */
